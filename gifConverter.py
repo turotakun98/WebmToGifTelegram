ddb5d38d0d950ba952af74bc88d52c86c0022f9c
turotakun98 <davideturini98@gmail.com>
2020-10-22 22:14:39 +0200

    Added gif in the message response

--- gifConverter.py
+++ gifConverter.py
@@ -1,23 +1,23 @@
 from telegram import Update, Message
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 import urllib.request
+import urllib
 import re
 import os
 import string
 import random
+import codecs
 
-
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+urlPattern = '^(http://www.|https://www.|http://|https://)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
+basePath = 'files/'
 
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def test(update: Update, context: CallbackContext) -> None:
-    regex = '^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$'
-    if re.match(regex, update.message.text) is not None:
+def messageReceiver(update: Update, context: CallbackContext) -> None:
+    if re.match(urlPattern, update.message.text) is not None:
         filename = id_generator(6)
         while(os.path.isfile(filename)):
             filename = id_generator(6)
@@ -25,20 +25,23 @@ def test(update: Update, context: CallbackContext) -> None:
         opener = urllib.request.URLopener()
         opener.addheader('User-Agent', 'whatever')
         opener.retrieve(
-            update.message.text, '/home/turotakun98/repo/gifConvertTG/{0}.webm'.format(filename))
-
+            update.message.text, '/home/turotakun98/repo/gifConvertTG/{0}{1}.webm'.format(basePath, filename))
+        msgLoading = update.message.reply_text('loading ...')
         os.system(
-            'ffmpeg -i {0}.webm -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 {0}.gif'.format(filename))
+            'ffmpeg -i {baseDir}{filename}.webm -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 {baseDir}{filename}.gif'.format(baseDir=basePath, filename=filename))
+
+        update.message.reply_animation(
+            animation=open(('{0}{1}.gif'.format(basePath, filename)), 'rb'))
+        msgLoading.delete()
 
     else:
-        print('invalid')
+        update.message.reply_text('Invalid url')
 
 
 updater = Updater('635156026:AAElQGaPkRKafynTGQh9kMjKj0tYoojdQs4')
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
 updater.dispatcher.add_handler(
-    MessageHandler(filters=Filters.all, callback=test))
+    MessageHandler(filters=Filters.all, callback=messageReceiver))
 
 updater.start_polling()
 updater.idle()
